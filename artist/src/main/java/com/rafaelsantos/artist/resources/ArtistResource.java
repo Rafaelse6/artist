b59f0c53491cc4189184f31a552221937f8a7028
Rafael Santos <rafa.05125@gmail.com>
2023-02-12 14:24:46 -0300

    ArtistResource - findAllPaged

---
+++ artist/src/main/java/com/rafaelsantos/artist/resources/ArtistResource.java
@@ -0,0 +1,26 @@
+package com.rafaelsantos.artist.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaelsantos.artist.DTO.ArtistDTO;
+import com.rafaelsantos.artist.services.ArtistService;
+
+@RestController
+@RequestMapping(value = "/artists")
+public class ArtistResource {
+	
+	@Autowired
+	private ArtistService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<ArtistDTO>> findAll(Pageable pageable){
+		Page<ArtistDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list);
+	}
+}

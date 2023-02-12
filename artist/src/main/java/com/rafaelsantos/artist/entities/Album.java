2007818222480cd07e34237883e3aa80c499163b
Rafael Santos <rafa.05125@gmail.com>
2023-02-12 16:22:17 -0300

    Album

---
+++ artist/src/main/java/com/rafaelsantos/artist/entities/Album.java
@@ -0,0 +1,88 @@
+package com.rafaelsantos.artist.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_album")
+public class Album implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name="tb_album_artist",
+			joinColumns = @JoinColumn(name = "album_id"),
+			inverseJoinColumns = @JoinColumn(name = "artist_id")
+	)
+	Set<Artist> artist = new HashSet<>();
+	
+	public Album() {}
+
+	public Album(Long id, String name, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.imgUrl = imgUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Artist> getArtist() {
+		return artist;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Album other = (Album) obj;
+		return Objects.equals(id, other.id);
+	}
+}
--- artist/src/main/resources/import.sql
+++ artist/src/main/resources/import.sql
@@ -1,3 +1,11 @@
 INSERT INTO tb_artist(name, country) VALUES ('Dream Theater', 'USA');
 INSERT INTO tb_artist(name, country) VALUES ('Sabaton', 'Sweden');
 INSERT INTO tb_artist(name, country) VALUES ('Sepultura', 'Brazil');
+
+INSERT INTO tb_album(name, img_Url) VALUES ('Images and Words', 'https://www.musik-sammler.de/cover/1500/1189_1564052579.jpg');
+INSERT INTO tb_album(name, img_Url) VALUES ('Heroes', 'https://4.bp.blogspot.com/-3BDsAt0INC4/U5duPxuYiEI/AAAAAAAACoI/6p-YShXFJMo/s1600/Sabaton_-_Heroes_-_Artwork.jpg');
+INSERT INTO tb_album(name, img_Url) VALUES ('Arise', 'https://1.bp.blogspot.com/-NKypTWD7nxk/UTWWfAoF34I/AAAAAAAAAGo/Sl1295mU7S0/s1600/url.jpg');
+
+INSERT INTO tb_album_artist(album_id, artist_id) VALUES (1,1);
+INSERT INTO tb_album_artist(album_id, artist_id) VALUES (2,2);
+INSERT INTO tb_album_artist(album_id, artist_id) VALUES (3,3);

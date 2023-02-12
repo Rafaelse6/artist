f5a4492c46769d76e6edd35926192f97c897ecd8
Rafael Santos <rafa.05125@gmail.com>
2023-02-12 14:22:27 -0300

    ArtistDTO

---
+++ artist/src/main/java/com/rafaelsantos/artist/DTO/ArtistDTO.java
@@ -0,0 +1,49 @@
+package com.rafaelsantos.artist.DTO;
+
+import com.rafaelsantos.artist.entities.Artist;
+
+public class ArtistDTO {
+	
+	private Long id;
+	private String name;
+	private String country;
+	
+	public ArtistDTO() {}
+	
+	public ArtistDTO(Long id, String name, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.country = country;
+	}
+
+	public ArtistDTO(Artist entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.country = entity.getCountry();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}

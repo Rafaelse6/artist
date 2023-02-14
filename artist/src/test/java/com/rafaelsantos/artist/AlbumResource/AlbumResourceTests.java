bbdf14ea8cea2f8caa753c01c6f05015eb77131c
Rafael Santos <rafa.05125@gmail.com>
2023-02-14 09:05:29 -0300

    AlbumResource - update tested

--- artist/src/test/java/com/rafaelsantos/artist/AlbumResource/AlbumResourceTests.java
+++ artist/src/test/java/com/rafaelsantos/artist/AlbumResource/AlbumResourceTests.java
@@ -1,8 +1,13 @@
 package com.rafaelsantos.artist.AlbumResource;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,10 +24,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rafaelsantos.artist.DTO.AlbumDTO;
 import com.rafaelsantos.artist.repositories.tests.Factory;
 import com.rafaelsantos.artist.resources.AlbumResource;
 import com.rafaelsantos.artist.services.AlbumService;
+import com.rafaelsantos.artist.services.exceptions.DatabaseException;
 import com.rafaelsantos.artist.services.exceptions.ResourceNotFoundException;
 
 @WebMvcTest(AlbumResource.class)
@@ -30,6 +37,9 @@ public class AlbumResourceTests {
 	
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	@MockBean
 	private AlbumService service;
@@ -40,12 +50,14 @@ public class AlbumResourceTests {
 	
 	private Long existingId;
 	private Long nonExistingId;
+	private Long dependentId;
 	
 	@BeforeEach
 	void setUp() throws Exception{
 		
 		existingId = 1L;
-		nonExistingId = 2L;
+		nonExistingId = 1000L;
+		dependentId = 2L;
 		
 		albumDto = Factory.createAlbumDto();
 		page = new PageImpl<>(List.of(albumDto));
@@ -54,8 +66,48 @@ public class AlbumResourceTests {
 		
 		when(service.findById(existingId)).thenReturn(albumDto);
 		when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+		
+		when(service.update(eq(existingId), any())).thenReturn(albumDto);
+		when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+	
+		doNothing().when(service).delete(existingId);
+		doThrow(ResourceNotFoundException.class).when(service).delete(nonExistingId);
+		doThrow(DatabaseException.class).when(service).delete(dependentId);
 	}
 	
+	
+	@Test
+	public void updateShouldReturnAlbumDTOWhenIdExists() throws Exception {
+
+		String jsonBody = objectMapper.writeValueAsString(albumDto);
+
+		ResultActions result = 
+				mockMvc.perform(put("/albums/{id}", existingId)
+					.content(jsonBody)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON));
+
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.imgUrl").exists());
+	}
+
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+
+		String jsonBody = objectMapper.writeValueAsString(albumDto);
+		
+		ResultActions result = 
+				mockMvc.perform(put("/albums/{id}", nonExistingId)
+					.content(jsonBody)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isNotFound());
+	}
 	
 	@Test
 	public void findByIdShouldReturnAlbumWhenIdExist() throws Exception{
